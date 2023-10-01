f6340145931cb7af6905fe8f86188ba0fb013e99
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2023-10-01 20:01:27 -0300

    Criação da interface gráfica: exemplo padrão com main() para teste

---
+++ interface.py
@@ -0,0 +1,30 @@
+
+from tkinter import *
+
+class Frame1(Frame):
+    def __init__(self, ws):
+        Frame.__init__(self, ws, bg="#006699")
+        self.ws = ws
+        self.widgets()
+
+    def widgets(self):
+        self.text = Label(self, text="This label is on the frame ")
+        self.text.grid(row=0, column=0, padx=20, pady=20) # margins
+
+
+class Win(Tk):
+
+    def __init__(self, ws):
+        Tk.__init__(self, ws)
+        self.ws = ws
+        self.title('Campo Minado')
+        self.geometry('500x500')
+        self.main()
+
+    def main(self):
+        self.w = Frame1(self)
+        self.w.pack()
+
+if __name__== "__main__":
+    app = Win(None)
+    app.mainloop()
