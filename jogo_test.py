42d7488148da7320b543469f658b2e9192493879
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2023-10-05 13:50:38 -0300

    Criação de testes para escolha de dificuldade

--- jogo_test.py
+++ jogo_test.py
@@ -6,3 +6,18 @@ def testeNivelAEscolher():
 
 def testeNivelEscolhido():
     assert main != ''
+
+def testeEscolherNivelFacil(): 
+    d = Dificuldade('fácil')
+
+    assert d.escolherNivel() == 'fácil'
+
+def testeEscolherNivelIntermediário(): 
+    d = Dificuldade('intermediário')
+
+    assert d.escolherNivel() == 'intermediário'
+
+def testeEscolherNiveDificil(): 
+    d = Dificuldade('difícil')
+
+    assert d.escolherNivel() == 'difícil'
