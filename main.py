1738ebf784ca1ebb34c84714ec05a1cacc53891f
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2023-10-06 16:31:51 -0300

    Criação de objeto de TelaDoJogo com parâmetros para fácil

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ from jogo.tabuleiro import Tabuleiro
 from views.interface import TelaDoJogo
 
 def main():
-    # d = Dificuldade('difícil')
-    # t = Tabuleiro(d.nivel)
-    # t.iniciarTabuleiro()
-    # t.mostrarTabuleiro()
-    t = TelaDoJogo() 
+    t = TelaDoJogo(8,8,10) 
     t.jogar()
    
 
