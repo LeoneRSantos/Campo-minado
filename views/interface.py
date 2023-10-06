7c66bea35389e574713ccebf5a505ec557b21946
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2023-10-05 23:01:27 -0300

    Refatoração em interface: criação da classe TelaDoJogo

---
+++ views/interface.py
@@ -0,0 +1,73 @@
+import random
+import emoji
+from tkinter import *
+
+class TelaDoJogo:
+    tela = Tk()
+
+    # tamanho do tabuleiro fácil
+    tabuleiro = [8, 8]
+    # Tamanho padrão de casas
+    tamanhoDaCasa = 32
+
+    tela.title("Campo Minado")
+    x = tabuleiro[0] * tamanhoDaCasa
+    y = tabuleiro[1] * tamanhoDaCasa
+
+    matrizDoJogo = []
+    # Casas randomizadas
+    casasRandomicas = []
+
+    # Função para verificar a casa
+    def verificarCasa(casaEspecifica):
+        xX = -1
+        yY = -1
+
+        for linhaAtual in range(len(TelaDoJogo.matrizDoJogo)): 
+            for colunaAtual in range(len(TelaDoJogo.matrizDoJogo[linhaAtual])):
+                if TelaDoJogo.matrizDoJogo[linhaAtual][colunaAtual] == casaEspecifica:
+                    xX = colunaAtual
+                    yY = linhaAtual
+                
+                
+        # print(f'Casa verificada: {xX},{yY} \t tem bomba? {casasRandomicas[xX][yY]}')
+
+        minado = TelaDoJogo.casasRandomicas[xX][yY]
+        if(minado):
+            casaEspecifica['text'] = '💣'
+            print('💣')
+            # emoji.emojize(":bomb:")
+        
+
+    # Geração do tabuleiro
+    tela.geometry(f"{x}x{y}")
+
+    for linha in range(tabuleiro[0]):
+        linhas = []
+        for coluna in range(tabuleiro[1]):
+
+            casa = Button(tela) 
+            casa['command'] = lambda casa = casa: TelaDoJogo.verificarCasa(casa)
+
+            posx = linha*tamanhoDaCasa
+            posy = coluna*tamanhoDaCasa
+            casa.place(x=posx, y=posy, width=tamanhoDaCasa, height=tamanhoDaCasa) 
+            linhas.append(casa)
+        matrizDoJogo.append(linhas)
+
+    for l in range(tabuleiro[0]):
+        novoValor = []
+        for c in range(tabuleiro[1]):
+            temBomba = bool(random.randint(0,1))
+            novoValor.append(temBomba)
+        casasRandomicas.append(novoValor)
+    print(casasRandomicas)
+
+
+    def jogar(self):
+
+        TelaDoJogo.tela.mainloop()
+
+
+
+
