0b338a2aea0a4a8e1a3e32dec60d76b35c5be7c3
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2023-10-13 16:41:57 -0300

    Testes para distribução de bombas maiores do que 10 no Fácil

---
+++ distribuicao_invalida_facil.py
@@ -0,0 +1,104 @@
+import pytest
+
+from views.tela_do_jogo import TelaDoJogo
+
+
+@pytest.mark.parametrize("matriz,linhas, colunas, esperado", [
+    ([[True, False, True, False, True, False, True, False],
+      [True, False, True, False, True, False, True, False],
+      [True, False, True, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False]], 8, 8, 10,
+     ),  
+
+     ([[False, False, False, False, True, False, True, False],
+      [True, False, True, False, True, False, True, False],
+      [True, False, True, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, True, True, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False]], 8, 8, 10,), 
+      
+      ([[False, False, False, False, True, False, True, False],
+      [True, False, True, False, True, False, True, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, True, True, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, True, True, False],
+      [False, False, False, False, False, False, False, False]], 8, 8, 10,), 
+      
+      ([[False, False, False, False, True, False, True, False],
+      [True, False, True, False, True, False, True, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, True, True, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, True, True, False, False, False]], 8, 8, 10,), 
+      
+      ([[False, False, False, False, True, False, True, False],
+      [True, False, True, False, True, False, True, False],
+      [False, False, False, False, False, False, True, True],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, True, True, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False]], 8, 8, 10,), 
+      
+      ([[False, False, False, False, False, False, False, False],
+      [True, False, True, False, True, False, True, False],
+      [True, False, True, True, True, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, True, True, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False]], 8, 8, 10,), 
+      
+      ([[False, False, False, False, True, False, True, False],
+      [False, False, False, False, True, False, True, False],
+      [True, False, True, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, True, True, False, False, False],
+      [False, True, True, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False]], 8, 8, 10,), 
+      
+      ([[False, False, False, False, True, False, True, False],
+      [True, False, True, False, True, False, True, False],
+      [True, False, True, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, True, True]], 8, 8, 10,), 
+      
+      ([[False, False, False, False, True, False, True, False],
+      [False, False, False, False, True, False, True, False],
+      [True, False, True, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, True, True, False, False, False, False],
+      [False, False, False, False, False, False, True, True]], 8, 8, 10,),
+      
+      ([[False, False, False, False, True, False, True, False],
+      [True, False, True, False, True, False, True, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [False, False, False, False, False, False, False, False],
+      [True, True, False, False, False, False, False, False],
+      [False, False, False, False, False, False, True, True]], 8, 8, 10,)])
+def testarCalcularNumeroDeBombasFacil(matriz, linhas, colunas, esperado):
+    t = TelaDoJogo(8,8,10)
+    cont = 1
+    for l in range(linhas):
+        for c in range(colunas):
+            if matriz[l][c] == True:
+              cont += 1
+    assert t.bombas < cont and t.bombas == esperado
