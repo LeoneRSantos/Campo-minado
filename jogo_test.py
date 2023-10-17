70b78883ac08eda5210ce093384b9c7bffbd5359
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2023-10-17 12:25:01 -0300

    Correção nos testes de jogo_test

--- jogo_test.py
+++ jogo_test.py
@@ -7,141 +7,174 @@ from tkinter import *
 
 tela = Tk()
 
+
 def teste_nivel_a_Escolher():
     assert Dificuldade.nivel == ''
-
-
-def teste_Tamanho_Da_Casa():
-    assert TelaDoJogo.tamanhoDaCasa == 28
-
-
-def teste_Nivel_Escolhido():
-    assert main != ''
 
 
 def teste_Escolher_Nivel_Facil():
     d = Dificuldade('fácil')
 
-    assert d.escolherNivel() == 'fácil'
+    assert d.escolherNivel() == d.nivel
 
 
-def teste_Escolher_Facil_Pelo_Clique():
-    d = Dificuldade('fácil')
-
-    TelaInicial.dificuldade = 'fácil'
-
-    assert d.nivel == TelaInicial.dificuldade
-
-
-def teste_Escolher_Inter_Pelo_Clique():
+def teste_Escolher_Nivel_Intermediario():
     d = Dificuldade('intermediário')
 
-    TelaInicial.dificuldade = 'intermediário'
-
-    assert d.nivel == TelaInicial.dificuldade
+    assert d.escolherNivel() == d.nivel
 
 
-def teste_Escolher_Dificil_Pelo_Clique():
+def teste_Escolher_Nivel_dificil():
     d = Dificuldade('difícil')
 
-    TelaInicial.dificuldade = 'difícil'
-
-    assert d.nivel == TelaInicial.dificuldade
+    assert d.escolherNivel() == d.nivel
 
 
 def testar_Se_Comeca_Com_Menu():
-    v = TelaInicial.comecar
+    TelaInicial.indicarComeco()
 
-    assert TelaInicial.indicarComeco() != v
-
-
-def teste_Escolher_Nivel_Intermediário():
-    d = Dificuldade('intermediário')
-
-    assert d.escolherNivel() == 'intermediário'
+    assert TelaInicial.comecar == True
 
 
-def teste_Escolher_Nivel_Dificil():
-    d = Dificuldade('difícil')
+def teste_Escolher_Facil_Pelo_Clique():
+    c = TelaInicial.comecar
+    TelaInicial.indicarComeco()
 
-    assert d.escolherNivel() == 'difícil'
+    TelaInicial.dificuldade = 'fácil'
+    d = Dificuldade(TelaInicial.dificuldade)
+
+    assert d.escolherNivel() == TelaInicial.retornarDificuldade() and c == True
+
+
+def teste_Escolher_Intermediario_Pelo_Clique():
+    TelaInicial.indicarComeco()
+
+    TelaInicial.dificuldade = 'fácil'
+    d = Dificuldade(TelaInicial.dificuldade)
+
+    assert d.escolherNivel() == TelaInicial.retornarDificuldade(
+    ) and TelaInicial.comecar == True
+
+
+def teste_Escolher_Dificil_Pelo_Clique():
+    TelaInicial.indicarComeco()
+    TelaInicial.dificuldade = 'difícil'
+
+    d = Dificuldade(TelaInicial.dificuldade)
+
+    assert d.escolherNivel() == TelaInicial.retornarDificuldade(
+    ) and TelaInicial.comecar == False
 
 
 def testar_Dimensoes_Do_Facil():
     t = TelaDoJogo(8, 8, 10, tela)
+    area = 0
 
-    assert t.linhas == 8 and t.colunas == 8
-
-
-def testar_Tabuleiro_Vazio_No_Facil():
-    t = TelaDoJogo(8, 8, 10,tela)
-
-    assert t.jogou == False
-
-
-def testar_Marcacao_Facil():
-    t = TelaDoJogo(8, 8, 10,tela)
-
-    assert t.casasRandomicas[2][2] == True or t.casasRandomicas[2][2] == False
-
-
-def testar_Marcacao_Dentr_oDo_Tabuleiro_Facil():
-    t = TelaDoJogo(8, 8, 10,tela)
-
-    for i in range(t.linhas):
+    for l in range(t.linhas):
         for c in range(t.colunas):
-            assert t.casasRandomicas[i][c] == True or t.casasRandomicas[i][c] == False
+            area += 1
+
+    assert t.linhas == 8 and t.colunas == 8 and area == 64
 
 
 def testar_Dimensoes_Intermediario():
-    t = TelaDoJogo(10,16,30,tela) 
-
-    contLinhas = 0
-    contColunas = 0
+    t = TelaDoJogo(10, 16, 30, tela)
+    area = 0
 
     for l in range(t.linhas):
-        contLinhas = l+1
         for c in range(t.colunas):
-            contColunas = c+1
+            area += 1
 
-    assert contLinhas == 10 and contColunas == 16 
+    assert t.linhas == 10 and t.colunas == 16 and area == 160
 
-def testar_Tabuleiro_Vazio_No_Intermediario():
-    t = TelaDoJogo(10, 16, 30,tela)
-
-    assert t.jogou == False
-
-def testar_Marcacao_Dentro_Do_Tabuleiro_Intermediario():
-    t = TelaDoJogo(10, 16, 30,tela)
-
-    for i in range(t.linhas):
-        for c in range(t.colunas):
-            assert t.casasRandomicas[i][c] == True or t.casasRandomicas[i][c] == False
-
-def testar_Marcacao_Intermediario():
-    t = TelaDoJogo(10, 16, 30,tela)
-
-    assert t.casasRandomicas[8][10] == True or t.casasRandomicas[8][10] == False
 
 def testar_Dimensoes_Dificil():
-    t = TelaDoJogo(24,24,100,tela) 
-
-    contLinhas = 0
-    contColunas = 0
+    t = TelaDoJogo(24, 24, 100, tela)
+    area = 0
 
     for l in range(t.linhas):
-        contLinhas = l+1
         for c in range(t.colunas):
-            contColunas = c+1
+            area += 1
 
-    assert contLinhas == 24 and contColunas == 24 
+    assert t.linhas == 24 and t.colunas == 24 and area == 576
+
+
+def testar_Tabuleiro_Vazio_No_Facil():
+    t = TelaDoJogo(8, 8, 10, tela)
+    vazio = True
+
+    for l in range(t.linhas):
+        for c in range(t.colunas):
+            if t.matrizDoJogo[l][c]['text'] == str(int):
+                vazio = False
+            elif t.matrizDoJogo[l][c]['text'] == 'B':
+                vazio = False
+            elif t.matrizDoJogo[l][c]['text'] == 'P':
+                vazio = False
+
+    assert vazio == True
+
+
+def testar_Tabuleiro_Vazio_No_Intermediario():
+    t = TelaDoJogo(10, 16, 30, tela)
+    vazio = True
+
+    for l in range(t.linhas):
+        for c in range(t.colunas):
+            if t.matrizDoJogo[l][c]['text'] == str(int):
+                vazio = False
+            elif t.matrizDoJogo[l][c]['text'] == 'B':
+                vazio = False
+            elif t.matrizDoJogo[l][c]['text'] == 'P':
+                vazio = False
+
+    assert vazio == True
+
 
 def testar_Tabuleiro_Vazio_No_Dificil():
-    t = TelaDoJogo(24, 24, 100,tela)
+    t = TelaDoJogo(24, 24, 100, tela)
+    vazio = True
 
-    assert t.jogou == False
+    for l in range(t.linhas):
+        for c in range(t.colunas):
+            if t.matrizDoJogo[l][c]['text'] == str(int):
+                vazio = False
+            elif t.matrizDoJogo[l][c]['text'] == 'B':
+                vazio = False
+            elif t.matrizDoJogo[l][c]['text'] == 'P':
+                vazio = False
+
+    assert vazio == True
+
+
+def testar_Marcacao_Facil():
+    t = TelaDoJogo(8, 8, 10, tela)
+
+    listaPossibilidades = ['0', '2', '3', '4', 'B', 'P']
+
+    for c in range(len(listaPossibilidades)):
+        t.matrizDoJogo[2][2]['text'] = listaPossibilidades[c]
+
+    assert t.matrizDoJogo[2][2]['text'] != ''
+
+
+def testar_Marcacao_Intermediario():
+    t = TelaDoJogo(10, 16, 30, tela)
+
+    listaPossibilidades = ['0', '2', '3', '4', 'B', 'P']
+
+    for c in range(len(listaPossibilidades)):
+        t.matrizDoJogo[8][14]['text'] = listaPossibilidades[c]
+
+    assert t.matrizDoJogo[8][14]['text'] != ''
+
 
 def testar_Marcacao_Dificil():
-    t = TelaDoJogo(24, 24, 100,tela)
+    t = TelaDoJogo(24, 24, 100, tela)
 
-    assert t.casasRandomicas[12][5] == True or t.casasRandomicas[12][5] == False
+    listaPossibilidades = ['0', '2', '3', '4', 'B', 'P']
+
+    for c in range(len(listaPossibilidades)):
+        t.matrizDoJogo[22][20]['text'] = listaPossibilidades[c]
+
+    assert t.matrizDoJogo[22][20]['text'] != ''
