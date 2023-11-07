3749566441420c421ac8df9e498819b97f677c1e
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2023-11-07 00:08:19 -0300

    Correções em continuar_jogo_apos_adicionar_bandeira_test

--- test/continuar_jogo_apos_adicionar_bandeira_test.py
+++ test/continuar_jogo_apos_adicionar_bandeira_test.py
@@ -4,35 +4,39 @@ from views.tela_do_jogo import TelaDoJogo
 
 tela = Tk()
 
-def testar_se_o_jogo_continua_apos_adicionar_bandeira_facil():
-    tf = TelaDoJogo(8,8,10,tela)
 
-    tf.adicionarBandeira(tf.matrizDoJogo[2][2])
-    tf.adicionarBandeira(tf.matrizDoJogo[1][2])
+def testar_se_o_jogo_continua_apos_adicionar_bandeira_facil():
+    tf = TelaDoJogo(8, 8, 10, tela)
+
+    tf.adicionarBandeira(2, 2)
+    tf.adicionarBandeira(2, 2)
 
     assert tf.perdeu == False
 
-def testar_se_o_jogo_continua_apos_adicionar_bandeira_intermediario():
-    ti = TelaDoJogo(10,16,30,tela)
 
-    ti.adicionarBandeira(ti.matrizDoJogo[4][2])
-    ti.adicionarBandeira(ti.matrizDoJogo[0][0])
+def testar_se_o_jogo_continua_apos_adicionar_bandeira_intermediario():
+    ti = TelaDoJogo(10, 16, 30, tela)
+
+    ti.adicionarBandeira(4, 2)
+    ti.adicionarBandeira(0, 0)
 
     assert ti.perdeu == False
 
-def testar_se_o_jogo_continua_apos_adicionar_bandeira_dificil():
-    td = TelaDoJogo(24,24,100,tela)
 
-    td.adicionarBandeira(td.matrizDoJogo[3][4])
-    td.adicionarBandeira(td.matrizDoJogo[5][2])
+def testar_se_o_jogo_continua_apos_adicionar_bandeira_dificil():
+    td = TelaDoJogo(24, 24, 100, tela)
+
+    td.adicionarBandeira(3, 4)
+    td.adicionarBandeira(5, 2)
 
     assert td.perdeu == False
 
+
 def testar_se_o_jogo_para_apos_adicionar_bandeira_facil():
-    tf = TelaDoJogo(8,8,10,tela)
+    tf = TelaDoJogo(8, 8, 10, tela)
     para = False
 
-    tf.adicionarBandeira(tf.matrizDoJogo[2][2])
+    tf.adicionarBandeira(2, 2)
 
     if tf.matrizDoJogo[2][2]['text'] == 'P':
         if tf.perdeu == True:
@@ -40,11 +44,12 @@ def testar_se_o_jogo_para_apos_adicionar_bandeira_facil():
 
     assert para == False
 
+
 def testar_se_o_jogo_para_apos_adicionar_bandeira_intermediario():
-    ti = TelaDoJogo(10,16,30,tela)
+    ti = TelaDoJogo(10, 16, 30, tela)
     para = False
 
-    ti.adicionarBandeira(ti.matrizDoJogo[7][8])
+    ti.adicionarBandeira(7, 8)
 
     if ti.matrizDoJogo[7][8]['text'] == 'P':
         if ti.perdeu == True:
@@ -52,11 +57,12 @@ def testar_se_o_jogo_para_apos_adicionar_bandeira_intermediario():
 
     assert para == False
 
+
 def testar_se_o_jogo_para_apos_adicionar_bandeira_dificil():
-    tf = TelaDoJogo(24,24,100,tela)
+    tf = TelaDoJogo(24, 24, 100, tela)
     para = False
 
-    tf.adicionarBandeira(tf.matrizDoJogo[20][18])
+    tf.adicionarBandeira(20, 18)
 
     if tf.matrizDoJogo[20][18]['text'] == 'P':
         if tf.perdeu == True:
