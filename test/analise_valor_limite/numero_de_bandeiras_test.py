d196a04bbb678081a18a824fd19d84e4fc28aa77
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2023-11-07 00:05:25 -0300

    Correções em numero_de_bandeiras_test

--- test/analise_valor_limite/numero_de_bandeiras_test.py
+++ test/analise_valor_limite/numero_de_bandeiras_test.py
@@ -14,10 +14,11 @@ def testar_numero_de_bandeiras_facil_igual_a_bombas():
     for l in range(t.linhas):
         for c in range(t.colunas):
             if t.casasRandomicas[l][c] == True:
-                t.adicionarBandeira(t.matrizDoJogo[l][c])
+                t.adicionarBandeira(l, c)
                 bandeiras += 1
 
     assert t.qtdBandeiras == bandeiras and t.bombas == bandeiras
+
 
 def testar_numero_de_bandeiras_facil_maior_que_bombas():
     t = TelaDoJogo(8, 8, 10, tela)
@@ -26,10 +27,11 @@ def testar_numero_de_bandeiras_facil_maior_que_bombas():
     for l in range(t.linhas):
         for c in range(t.colunas):
             if t.casasRandomicas[l][c] == True:
-                t.adicionarBandeira(t.matrizDoJogo[l][c])
+                t.adicionarBandeira(l, c)
                 bandeiras += 1
 
     assert not t.qtdBandeiras > bandeiras and not t.bombas > bandeiras
+
 
 def testar_numero_de_bandeiras_facil_menor_que_bombas():
     t = TelaDoJogo(8, 8, 10, tela)
@@ -38,7 +40,7 @@ def testar_numero_de_bandeiras_facil_menor_que_bombas():
     for l in range(t.linhas):
         for c in range(t.colunas):
             if t.casasRandomicas[l][c] == True:
-                t.adicionarBandeira(t.matrizDoJogo[l][c])
+                t.adicionarBandeira(l, c)
                 bandeiras += 1
 
     assert not t.qtdBandeiras < bandeiras and not t.bombas < bandeiras
@@ -51,10 +53,11 @@ def testar_numero_de_bandeiras_intermediario_igual_a_bombas():
     for l in range(t.linhas):
         for c in range(t.colunas):
             if t.casasRandomicas[l][c] == True:
-                t.adicionarBandeira(t.matrizDoJogo[l][c])
+                t.adicionarBandeira(l, c)
                 bandeiras += 1
 
     assert t.qtdBandeiras == bandeiras and t.bombas == bandeiras
+
 
 def testar_numero_de_bandeiras_intermediario_maior_que_bombas():
     t = TelaDoJogo(10, 16, 30, tela)
@@ -63,10 +66,11 @@ def testar_numero_de_bandeiras_intermediario_maior_que_bombas():
     for l in range(t.linhas):
         for c in range(t.colunas):
             if t.casasRandomicas[l][c] == True:
-                t.adicionarBandeira(t.matrizDoJogo[l][c])
+                t.adicionarBandeira(l, c)
                 bandeiras += 1
 
     assert not t.qtdBandeiras > bandeiras and not t.bombas > bandeiras
+
 
 def testar_numero_de_bandeiras_intermediario_menor_que_bombas():
     t = TelaDoJogo(10, 16, 30, tela)
@@ -75,7 +79,7 @@ def testar_numero_de_bandeiras_intermediario_menor_que_bombas():
     for l in range(t.linhas):
         for c in range(t.colunas):
             if t.casasRandomicas[l][c] == True:
-                t.adicionarBandeira(t.matrizDoJogo[l][c])
+                t.adicionarBandeira(l, c)
                 bandeiras += 1
 
     assert not t.qtdBandeiras < bandeiras and not t.bombas < bandeiras
@@ -88,10 +92,11 @@ def testar_numero_de_bandeiras_dificil_igual_a_bombas():
     for l in range(t.linhas):
         for c in range(t.colunas):
             if t.casasRandomicas[l][c] == True:
-                t.adicionarBandeira(t.matrizDoJogo[l][c])
+                t.adicionarBandeira(l, c)
                 bandeiras += 1
 
     assert t.qtdBandeiras == bandeiras and t.bombas == bandeiras
+
 
 def testar_numero_de_bandeiras_dificil_maior_que_bombas():
     t = TelaDoJogo(24, 24, 100, tela)
@@ -100,10 +105,11 @@ def testar_numero_de_bandeiras_dificil_maior_que_bombas():
     for l in range(t.linhas):
         for c in range(t.colunas):
             if t.casasRandomicas[l][c] == True:
-                t.adicionarBandeira(t.matrizDoJogo[l][c])
+                t.adicionarBandeira(l, c)
                 bandeiras += 1
 
     assert not t.qtdBandeiras > bandeiras and not t.bombas > bandeiras
+
 
 def testar_numero_de_bandeiras_dificil_menor_que_bombas():
     t = TelaDoJogo(24, 24, 100, tela)
@@ -112,7 +118,7 @@ def testar_numero_de_bandeiras_dificil_menor_que_bombas():
     for l in range(t.linhas):
         for c in range(t.colunas):
             if t.casasRandomicas[l][c] == True:
-                t.adicionarBandeira(t.matrizDoJogo[l][c])
+                t.adicionarBandeira(l, c)
                 bandeiras += 1
 
     assert not t.qtdBandeiras < bandeiras and not t.bombas < bandeiras
