01df2190b971dec4746c7ba017ceac111db3c7da
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2023-11-08 01:28:21 -0300

    Modificações gerais no projeto

--- test/analise_valor_limite/numero_de_bombas_no_tab_test.py
+++ test/analise_valor_limite/numero_de_bombas_no_tab_test.py
@@ -3,11 +3,10 @@ from views.tela_do_jogo import TelaDoJogo
 
 tela = Tk()
 
-# Verificar se o número de bombas do tabuleiro é maior que 10 [x]
-# Verificar se o número de bombas do tabuleiro é menor que 10 [x]
-# Verificar se o número de bombas é exatamente 10.
+# verificar se o número de bombas do tabuleiro é maior que 10 [x]
+#   verificar se o número de bombas do tabuleiro é menor que 10 [x]
 
-def testar_se_o_numero_de_bombas_e_menor_que_dez_facil():
+def testar_se_o_numero_de_bombas_esta_correto_Facil_menor():
     t = TelaDoJogo(8,8,10,tela)
     qdtBombas = 0
 
@@ -19,7 +18,7 @@ def testar_se_o_numero_de_bombas_e_menor_que_dez_facil():
     assert not qdtBombas < 10 
 
 
-def testar_se_o_numero_de_bombas_e_maior_que_dez_facil():
+def testar_se_o_numero_de_bombas_esta_correto_Facil_maior():
     t = TelaDoJogo(8,8,10,tela)
     qdtBombas = 0
 
@@ -30,19 +29,8 @@ def testar_se_o_numero_de_bombas_e_maior_que_dez_facil():
 
     assert not qdtBombas > 10
 
-def testar_se_o_numero_de_bombas_e_exatamente_dez_facil():
-    t = TelaDoJogo(8,8,10,tela)
-    qdtBombas = 0
 
-    for linhas in range(t.linhas):
-        for colunas in range(t.colunas):
-            if t.casasRandomicas[linhas][colunas] == True:
-                qdtBombas += 1
-
-    assert qdtBombas == 10
-
-
-def testar_se_o_numero_de_bombas_e_menor_que_trinta_intermediario():
+def testar_se_o_numero_de_bombas_esta_correto_Intermediario_menor():
     t = TelaDoJogo(10,16,30,tela)
     qdtBombas = 0
 
@@ -53,7 +41,7 @@ def testar_se_o_numero_de_bombas_e_menor_que_trinta_intermediario():
 
     assert not qdtBombas < 30
 
-def testar_se_o_numero_de_bombas_e_maior_que_trinta_intermediario():
+def testar_se_o_numero_de_bombas_esta_correto_Intermediario_maior():
     t = TelaDoJogo(10,16,30,tela)
     qdtBombas = 0
 
@@ -64,18 +52,7 @@ def testar_se_o_numero_de_bombas_e_maior_que_trinta_intermediario():
 
     assert not qdtBombas > 30
 
-def testar_se_o_numero_de_bombas_e_exatamente_igual_a_trinta_intermediario():
-    t = TelaDoJogo(10,16,30,tela)
-    qdtBombas = 0
-
-    for linhas in range(t.linhas):
-        for colunas in range(t.colunas):
-            if t.casasRandomicas[linhas][colunas] == True:
-                qdtBombas += 1
-
-    assert qdtBombas == 30
-
-def testar_se_o_numero_de_bombas_e_menor_que_cem_dificil():
+def testar_se_o_numero_de_bombas_esta_correto_Dificil_menor():
     t = TelaDoJogo(24,24,100,tela)
     qdtBombas = 0
 
@@ -86,7 +63,7 @@ def testar_se_o_numero_de_bombas_e_menor_que_cem_dificil():
 
     assert not qdtBombas < 100 
 
-def testar_se_o_numero_de_bombas_e_maior_que_cem_dificil():
+def testar_se_o_numero_de_bombas_esta_correto_Dificil_maior():
     t = TelaDoJogo(24,24,100,tela)
     qdtBombas = 0
 
@@ -96,14 +73,3 @@ def testar_se_o_numero_de_bombas_e_maior_que_cem_dificil():
                 qdtBombas += 1
 
     assert not qdtBombas > 100 
-
-def testar_se_o_numero_de_bombas_e_exatamente_igual_a_cem_dificil():
-    t = TelaDoJogo(24,24,100,tela)
-    qdtBombas = 0
-
-    for linhas in range(t.linhas):
-        for colunas in range(t.colunas):
-            if t.casasRandomicas[linhas][colunas] == True:
-                qdtBombas += 1
-
-    assert qdtBombas == 100 
--- test/testes.md
+++
@@ -1,5 +0,0 @@
-# Para executar os testes
-```sh
-python3 -m pytest ./test/classes_de_equivalencia/
-```
-- python3 -m pytest ./test/classes_de_equivalencia/
